6bacfa313c86a9704e67d86e50efac70fbfa8707
Levi Pereira <levi.pereira@gmail.com>
2024-04-04 18:21:52 -0300

    Added Install dependencies

---
+++ install_dependencies.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Update package lists
+apt-get update
+
+# Install required system dependencies
+apt-get install -y zip htop screen libgl1-mesa-glx libfreetype6-dev
+
+# Clean up
+apt-get clean
+rm -rf /var/lib/apt/lists/*
+
+# Upgrade pip
+pip install --upgrade pip
+
+# Install necessary Python packages
+pip install seaborn \
+            thop \
+            markdown-it-py==2.2.0 \
+            onnx-simplifier==0.4.35 \
+            onnxsim==0.4.35 \
+            onnxruntime==1.16.3 \
+            ujson==5.9.0 \
+            pycocotools==2.0.7 \
+            --no-cache-dir --force-reinstall
+
+# Install specific version of Pillow with no cache directory and force reinstall
+pip install pillow==9.5.0 --no-cache-dir --force-reinstall
