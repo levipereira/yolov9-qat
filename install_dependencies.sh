98d4521cd5f9f81429dfbce35c7a688ce7c17a42
Levi Pereira <levi.pereira@gmail.com>
2024-06-26 17:20:26 -0300

    Upgrade TensorRT from 10.0 to 10.1

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -43,17 +43,19 @@ if $defaults; then
 
 
     # upgrade TensorRT from 8.5.3 cuda 11.8 to 10.0 Cuda 11.2
-    wget https://developer.nvidia.com/downloads/compute/machine-learning/tensorrt/10.0.1/local_repo/nv-tensorrt-local-repo-ubuntu2004-10.0.1-cuda-12.4_1.0-1_amd64.deb || exit 1
-    dpkg -i nv-tensorrt-local-repo-ubuntu2004-10.0.1-cuda-12.4_1.0-1_amd64.deb || exit 1
-    cp /var/nv-tensorrt-local-repo-ubuntu2004-10.0.1-cuda-12.4/nv-tensorrt-local-4BE0C9B6-keyring.gpg /usr/share/keyrings/ || exit 1
+    
+    wget https://developer.download.nvidia.com/compute/machine-learning/tensorrt/10.1.0/local_repo/nv-tensorrt-local-repo-ubuntu2004-10.1.0-cuda-12.4_1.0-1_amd64.deb || exit 1
+    dpkg -i nv-tensorrt-local-repo-ubuntu2004-10.1.0-cuda-12.4_1.0-1_amd64.deb || exit 1
+    cp /var/nv-tensorrt-local-repo-ubuntu2004-10.1.0-cuda-12.4/*keyring.gpg /usr/share/keyrings/ || exit 1
     apt-get update || exit 1
-    apt-get install -y tensorrt onnx-graphsurgeon || exit 1
+    apt-get install -y tensorrt || exit 1
     apt-get purge "nv-tensorrt-local-repo*" -y || exit 1
-    rm -f nv-tensorrt-local-repo-ubuntu2004-10.0.1-cuda-12.4_1.0-1_amd64.deb
+    rm -f nv-tensorrt-local-repo-ubuntu2004-10.1.0-cuda-12.4_1.0-1_amd64.deb
     # Upgrade pip
     pip install --upgrade pip || exit 1
-
+    pip install --upgrade tensorrt==10.1.0 || exit 1
     # Install necessary Python packages
+    
     pip install seaborn \
                 thop \
                 markdown-it-py==2.2.0 \
@@ -64,7 +66,6 @@ if $defaults; then
                 pycocotools==2.0.7 \
                 pycuda==2024.1  || exit 1
     pip install onnx_graphsurgeon --extra-index-url https://pypi.ngc.nvidia.com  || exit 1
-    pip install --upgrade tensorrt==10.0.1 || exit 1
     pip install pillow==9.5.0 --no-cache-dir --force-reinstall || exit 1
 fi
 
